50f6ce9466a2e879743ae3785f5f8aa769d24349
pr0toshi <46229158+pr0toshi@users.noreply.github.com>
2023-07-08 16:38:39 +0000

    tt

--- src/App.js
+++ src/App.js
@@ -76,10 +76,10 @@ function App() {
 
   return (
     <div style={{ color: '#00ff55' }} className="bg-gray-900 h-full w-full min-h-screen" >
-      <div style={{ color: '#00ff55' }} className="flex flex-col bg-gray-800 space-y-6 mx-6 justify-center m-auto max-w-4xl min-w-80 shadow-md rounded-md border border-solid border-green-500 overflow-hidden">
+      <div style={{ color: '#00ff55' }} className="flex flex-col bg-gray-800 space-y-6 justify-center m-auto max-w-4xl min-w-80 shadow-md rounded-md border border-solid border-green-500 overflow-hidden">
         <h1 className="m-auto text-center md:mt-8 color-green-500 text-2xl md:text-3xl font-extrabold w-3/4">
           Welcome to Morpheus
-        </h1><div style={{ color: '#77ff8b' }}>
+        </h1><div style={{ color: '#77ff8b' }}className='mx-6' >
           Morpheus is designed to create a fully decentralized data market, allowing anyone to host an oracle and anyone to request data for a fee, creating a free and open data market. Oracles can stake $SCRY to create economic incentives to provide honest data to not be slashed. Anyone can run a Scry Morpheus node and help the network and developers access data when they need it.
         </div>
         <Button onClick={() => window.location.assign('https://docs.scry.finance/docs/morpheus/morpehus')} style={{ color: '#77ff8b' }} variant='outlined' className="m-auto text-center bottom-4 color-green-500 border-green-500">Morpheus Docs</Button>
