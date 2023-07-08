4b77ba4662ec43f54ca7ab26d71b87cec5dd9585
pr0toshi <46229158+pr0toshi@users.noreply.github.com>
2023-07-08 15:42:31 +0000

    tt

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,8 @@
-const colors = require('tailwindcss/colors')
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: 'class', // or 'media' or 'class'
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {
-      keyframes: {
-        rotate: {
-          '100%': {
-            transform: 'rotate(360deg)',
-          },
-        },
-        dash: {
-          '0%': {
-            strokeDasharray: '0, 150',
-            opacity: '1',
-            stroke: '#2185d0',
-          },
-          '100%': {
-            strokeDasharray: '120',
-          },
-        },
-      },
-      animation: {
-        spinnerTimer: 'dash 5s linear infinite',
-        rainbow: 'rainbow 5s linear infinite',
-      },
-    },
-  },
-  variants: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/forms')],
+  plugins: [],
 }
