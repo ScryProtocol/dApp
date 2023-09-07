22164a3cff3e635ce066c55adfd7505a415d3cee
pr0toshi <46229158+pr0toshi@users.noreply.github.com>
2023-09-07 09:00:23 +0000

    tt

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,7 @@ const Home: NextPage = () => {
       <Head>
         <title>Veryfi</title>
         <meta name="description" content="Verify that you own any NFTs, ERCs, are staking or any other ownership that uses balanceOf, for any other network! Veryfi lets you easily check membership and ownership for your community/project!" />
-        <link rel="icon" href="./public/veryfi.png" />
+        <link rel="icon" href="/veryfi.png" />
       </Head>
       <Toaster />
 
