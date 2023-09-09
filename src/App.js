2d76ec77376e3910b173700c917d52492ab6e073
pr0toshi <46229158+pr0toshi@users.noreply.github.com>
2023-09-09 20:37:53 +0000

    tt

--- src/App.js
+++ src/App.js
@@ -261,7 +261,7 @@ function App() {
             }
           });
           console.log(res.data.IpfsHash);
-          await setIPFS(res.data.IpfsHash)//IPFS = res.data.IpfsHash)
+          setIPFS(res.data.IpfsHash)//IPFS = res.data.IpfsHash)
         } catch (error) {
           console.log(error);
         }
@@ -420,8 +420,7 @@ function App() {
           </Select>
           <h3>Token</h3>
           <input type="text" style={{ backgroundColor: '#00ccff', right: '2px' }} placeholder="Token" value={ID} onChange={(e) => { setID(e.target.value) }} className=" w-80 text-center flex flex-col justify-center m-auto max-w-4xl min-w-80 shadow-md rounded-md border border-solid border-white overflow-hidden" />
-          <Button style={{ backgroundColor: '#00aaff', color: '#ffffff' }} variant='outlined' className="top-2 color-white border-white" onClick={sign}>Refresh</Button>
-<Button style={{ backgroundColor: '#00aaff', color: '#ffffff' }} variant='outlined' className="top-2 color-white border-white" onClick={LOL}>Refresh</Button>
+          <Button style={{ backgroundColor: '#00aaff', color: '#ffffff' }} variant='outlined' className="top-2 color-white border-white" onClick={LOL}>Refresh</Button>
           <div></div></div>Please note there is a 0.001 ETH fee.</div >
       <div>
       </div>
