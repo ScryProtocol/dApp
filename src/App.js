03c0dd15d00829698e993bb81e7644d90049a54f
pr0toshi <46229158+pr0toshi@users.noreply.github.com>
2023-09-10 09:26:36 +0000

    tt

--- src/App.js
+++ src/App.js
@@ -457,7 +457,7 @@ function App() {
           <h3>Token</h3>
           <input type="text" style={{ backgroundColor: '#00ccff', right: '2px' }} placeholder="Token" value={ID} onChange={(e) => { setID(e.target.value) }} className=" w-80 text-center flex flex-col justify-center m-auto max-w-4xl min-w-80 shadow-md rounded-md border border-solid border-white overflow-hidden" />
           <Button style={{ backgroundColor: '#00aaff', color: '#ffffff' }} variant='outlined' className="top-2 color-white border-white" onClick={LOL}>Refresh</Button>
-          <div></div></div>Please note there is a 0.001 ETH fee.</div >
+          <div></div></div></div >
       <div>
       </div>
     </div >
