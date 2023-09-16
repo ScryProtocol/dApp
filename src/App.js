07850a1d3dc19da7ae7d87cd3efb8356d3cb83c4
pr0toshi <46229158+pr0toshi@users.noreply.github.com>
2023-09-16 21:04:46 +0000

    tt

--- src/App.js
+++ src/App.js
@@ -746,7 +746,7 @@ function App() {
             <Button style={{ backgroundColor: '#00aaff', color: '#ffffff' }} variant='outlined' className="top-3 left-4 color-white border-white" onClick={() => {
               let currentURL = new URL(window.location.href);
               currentURL = currentURL.origin; navigator.clipboard.writeText(currentURL + '?NFT=' + addrs + '&ID=' + ID); toast.success("Copied :)")
-            }}>share signing link</Button>
+            }}>copy link</Button>
             <div>.</div></div></div></div >
       <div>.
       </div>
