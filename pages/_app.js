fc2a6c95c7049c1aa6c79e2a92cc37021ead5afa
pr0toshi <46229158+pr0toshi@users.noreply.github.com>
2023-09-16 17:17:21 +0000

    tt

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import {
   getDefaultWallets,
   RainbowKitProvider,
 } from '@rainbow-me/rainbowkit';
-import { configureChains, createConfig, sepolia, WagmiConfig } from 'wagmi';
+import { configureChains, createConfig, WagmiConfig } from 'wagmi';
 import {
   sepolia,
   polygon,
