57c5b36d560bb53240c6d556e4c2ca9322404249
pr0toshi <46229158+pr0toshi@users.noreply.github.com>
2023-09-18 16:13:34 +0000

    tt

--- pages/_app.js
+++ pages/_app.js
@@ -16,7 +16,7 @@ import {
 import { alchemyProvider } from 'wagmi/providers/alchemy';
 import { publicProvider } from 'wagmi/providers/public';
 const { chains, publicClient } = configureChains(
-  [sepolia,mainnet,  optimism, arbitrum, base],
+  [sepolia,  optimism, arbitrum, base],
   [
     //alchemyProvider({ apiKey: 'noFpU53uptypQtmZDodsoYQwqcK2V3AC' }),
     publicProvider()
