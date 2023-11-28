893a89fd5022aac183bbfbd85f9d835ada18dbde
pr0toshi <46229158+pr0toshi@users.noreply.github.com>
2023-11-28 16:54:48 +0000

    tt

--- pages/index.jsx
+++ pages/index.jsx
@@ -11,11 +11,11 @@ export default function Home() {
     <meta name="theme-color" content="#000000" />
     <meta
       name="description"
-      content="Welcome to Signet, sign NFTs for all your fans, collect signatures from your favourite creators and show off all your signats!"
+      content="Welcome to Collab by Signet, Collab allows you to engage in a new, more personal way, and collectors to have a whole new dimension of value and collectability. The Collabs a constantly evolving fully onchain and decentralized space to collaboratively draw and contribute to a single art piece, with each snapshot NFT showing a moment in time, with fully onchain authentication for those who contributed using IPFS attestation and layering. Draw, collaborate, collect."
     />
     <link rel="apple-touch-icon" href="./pub/logo.png" />
     
-    <title>Signet</title>
+    <title>Collab</title>
   </head>
         <App />
     </>
