daa9b62384f78090dfdb396c74629ae0669c4dac
pr0toshi <46229158+pr0toshi@users.noreply.github.com>
2024-01-14 04:25:27 +0000

    tt

--- pages/index.jsx
+++ pages/index.jsx
@@ -11,11 +11,11 @@ export default function Home() {
     <meta name="theme-color" content="#000000" />
     <meta
       name="description"
-      content="Welcome to Collab by Signet, Collab allows you to engage in a new, more personal way, and collectors to have a whole new dimension of value and collectability. The Collabs a constantly evolving fully onchain and decentralized space to collaboratively draw and contribute to a single art piece, with each snapshot NFT showing a moment in time, with fully onchain authentication for those who contributed using IPFS attestation and layering. Draw, collaborate, collect."
+      content="The DeathRoll smart contract is a PvP betting game where the outcome is determined by rolls."
     />
     <link rel="apple-touch-icon" href="./pub/logo.png" />
     
-    <title>Collab</title>
+    <title>Death Roll</title>
   </head>
         <App />
     </>
