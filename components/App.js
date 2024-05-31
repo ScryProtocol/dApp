4f17625a4fc422a2f68200fa91844b07d3398bf0
pr0toshi <46229158+pr0toshi@users.noreply.github.com>
2024-05-31 09:39:25 +0000

    tt

--- components/App.js
+++ components/App.js
@@ -329,11 +329,13 @@ const App = () => {
     const lenderAddress = userAddress
     let    streamContractAddress = ChainId === 1 ? '0x90076e40a74f33cc2c673ecbf2fba4068af77892' : '0x12A5103f551Fe9B659Fb40DCd4701CbE1bA0B3e6';
     let contract = new ethers.Contract(streamContractAddress, streamContractABI, provider);
-    const allowances = await contract.viewLenderAllowances(lenderAddress);
-
+    const allowances = await contract.viewLenderAllowances('0x849151d7D0bF1F34b70d5caD5149D28CC2308bf1');
     const subscriptionsData = await Promise.all(
       allowances.map(async (allowance) => {
+        console.log(allowance)
+
         const details = await contract.streamDetails(allowance);
+        console.log( details.token)
         const tokenContract = new ethers.Contract(
           details.token,
           [
