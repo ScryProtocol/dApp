ff918af7928de3f577d3b4b1e2900c16dce6ba27
pr0toshi <46229158+pr0toshi@users.noreply.github.com>
2024-10-04 12:17:05 +0000

    tt

--- components/App.js
+++ components/App.js
@@ -168,7 +168,7 @@ const App = () => {
     '0x833589fcd6edb6e08f4c7c32d4f71b54bda02913' : 'USDC' });
   function checkToken(token) {
     if (maps[token.toLowerCase()] != null) {
-      return maps[token];
+      return maps[token.toLowerCase()];
     }
     return token;
   }
