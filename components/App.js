23d7480bf7976de5a0be0b3dc9bdffd088aa9f8e
pr0toshi <46229158+pr0toshi@users.noreply.github.com>
2024-12-03 15:43:33 +0000

    tt

--- components/App.js
+++ components/App.js
@@ -646,7 +646,7 @@ const App = () => {
           border: '1px solid #ddd',
           padding: '12px',
           backgroundColor: '#f2f2f2',
-          textAlign: 'left',
+          textAlign: 'center',
         }}
       />
     ),
@@ -655,7 +655,7 @@ const App = () => {
         {...props}
         style={{
           border: '1px solid #ddd',
-          padding: '12px',
+          padding: '12px', textAlign: 'center',
         }}
       />
     ),
