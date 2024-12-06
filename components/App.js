86f8cda620280321e0a29c485e68d1e3ef0335ea
pr0toshi <46229158+pr0toshi@users.noreply.github.com>
2024-12-06 10:52:45 +0000

    tt

--- components/App.js
+++ components/App.js
@@ -572,7 +572,7 @@ const App = () => {
     // Add more navigation items if needed
   ];
 
-  const topwikiPages = ['home', 'source', 'directory', 'crypto101', 'how%20to'];
+  const topwikiPages = ['home', 'source', 'directory', 'crypto101', 'how to'];
 
   // Maintenance Navigation Items
   const maintNavigation = [
