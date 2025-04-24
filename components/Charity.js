f26e5655047f7a9ae0c447e3b25cc5f9d5800dab
pr0toshi <46229158+pr0toshi@users.noreply.github.com>
2025-04-24 10:06:14 +0000

    tt

--- components/Charity.js
+++ components/Charity.js
@@ -81,7 +81,7 @@ const defaultCharities = [
       {
         token: 'USDC',
         address: '0x833589fcd6edb6e08f4c7c32d4f71b54bda02913',
-        presetAmounts: [5, 10, 20],
+        presetAmounts: [10, 20,50],
       },
       {
         token: 'wETH',
