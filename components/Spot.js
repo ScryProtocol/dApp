e8895f279e65e16f408cbcb72a2d68f8f8fde11f
pr0toshi <46229158+pr0toshi@users.noreply.github.com>
2025-06-19 16:00:24 +0000

    tt

--- components/Spot.js
+++ components/Spot.js
@@ -1564,8 +1564,8 @@ staking: {
         {/* Sidebar / header */}
         <div
           className={getThemeClass(
-            "w-full md:w-1/3 md:border-b-0 md:border-r border-gray-200 overflow-y-auto rounded-t-3xl md:rounded-l-3xl md:rounded-tr-none",
-            "w-full md:w-1/3 border-b md:border-b-0 md:border-r border-gray-600 overflow-y-auto rounded-t-3xl md:rounded-l-3xl md:rounded-tr-none",
+            "hidden md:block w-full md:w-1/3 md:border-b-0 md:border-r border-gray-200 overflow-y-auto rounded-t-3xl md:rounded-l-3xl md:rounded-tr-none",
+            "hidden md:block w-full md:w-1/3 border-b md:border-b-0 md:border-r border-gray-600 overflow-y-auto rounded-t-3xl md:rounded-l-3xl md:rounded-tr-none",
             darkMode
           )}
         >
@@ -1682,6 +1682,74 @@ staking: {
         {/* Main content */}
         <div className="w-full md:w-2/3 py-2 p-6 md:p-8 overflow-y-auto">
         
+          <div
+            className={getThemeClass(
+              "block md:hidden flex items-center justify-center my-4 mb-2 gap-3 bg-gray-100 px-4 py-2 rounded-full w-fit mx-auto",
+              "block md:hiddenflex items-center justify-center my-4 mb-2 gap-3 bg-gray-700 px-4 py-2 rounded-full w-fit mx-auto",
+              darkMode
+            )}
+          >
+            {/* Twitter */}
+            <a
+              href="https://twitter.com/not_pr0"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={getThemeClass(
+                "text-blue-600 hover:text-blue-700 transition-colors",
+                "text-blue-400 hover:text-blue-300 transition-colors",
+                darkMode
+              )}
+            >
+              <svg
+                role="img"
+                fill={darkMode ? "#fff" : "#ec4899"}
+                className="color-blue-100 w-6 h-6"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-6 h-6"
+              >
+                <path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z" />
+              </svg>
+            </a>
+            {/* Discord */}
+            <a
+              href="https://discord.gg/vrV4YpUccq"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={getThemeClass(
+                "text-blue-600 hover:text-blue-700 transition-colors",
+                "text-blue-400 hover:text-blue-300 transition-colors",
+                darkMode
+              )}
+            >
+              <svg
+                role="img"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-6 h-6"
+                fill={darkMode ? "#fff" : "#ec4899"}
+              >
+                <path d="M20.317 4.3698a19.7913 19.7913 0 00-4.8851-1.5152.0741.0741 0 00-.0785.0371c-.211.3753-.4447.8648-.6083 1.2495-1.8447-.2762-3.68-.2762-5.4868 0-.1636-.3933-.4058-.8742-.6177-1.2495a.077.077 0 00-.0785-.037 19.7363 19.7363 0 00-4.8852 1.515.0699.0699 0 00-.0321.0277C.5334 9.0458-.319 13.5799.0992 18.0578a.0824.0824 0 00.0312.0561c2.0528 1.5076 4.0413 2.4228 5.9929 3.0294a.0777.0777 0 00.0842-.0276c.4616-.6304.8731-1.2952 1.226-1.9942a.076.076 0 00-.0416-.1057c-.6528-.2476-1.2743-.5495-1.8722-.8923a.077.077 0 01-.0076-.1277c.1258-.0943.2517-.1923.3718-.2914a.0743.0743 0 01.0776-.0105c3.9278 1.7933 8.18 1.7933 12.0614 0a.0739.0739 0 01.0785.0095c.1202.099.246.1981.3728.2924a.077.077 0 01-.0066.1276 12.2986 12.2986 0 01-1.873.8914.0766.0766 0 00-.0407.1067c.3604.698.7719 1.3628 1.225 1.9932a.076.076 0 00.0842.0286c1.961-.6067 3.9495-1.5219 6.0023-3.0294a.077.077 0 00.0313-.0552c.5004-5.177-.8382-9.6739-3.5485-13.6604a.061.061 0 00-.0312-.0286zM8.02 15.3312c-1.1825 0-2.1569-1.0857-2.1569-2.419 0-1.3332.9555-2.4189 2.157-2.4189 1.2108 0 2.1757 1.0952 2.1568 2.419 0 1.3332-.9555 2.4189-2.1569 2.4189zm7.9748 0c-1.1825 0-2.1569-1.0857-2.1569-2.419 0-1.3332.9554-2.4189 2.1569-2.4189 1.2108 0 2.1757 1.0952 2.1568 2.419 0 1.3332-.946 2.4189-2.1568 2.4189Z" />
+              </svg>
+            </a>
+            {/* Basescan */}
+            <a
+              href="https://basescan.org/address/0xa5d97df3b74019d794cafbaF2d63Cc56250a8dF7"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={getThemeClass(
+                "text-blue-600 hover:text-blue-700 transition-colors",
+                "text-blue-400 hover:text-blue-300 transition-colors",
+                darkMode
+              )}
+            >
+              <img
+                src="https://basescan.org/assets/base/images/svg/brandassets/logo-symbol.svg?v=25.1.4.0"
+                alt="basescan"
+                className="w-6 h-6 inline-block"
+              />
+            </a>
+          </div>
           <div className="px-6 pb-2 md:hidden">
             <select
               value={activeTab}
